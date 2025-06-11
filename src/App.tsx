624dc54a56fabdfe0f94da6f37091b897e89bee9
RezaBafandeh <165043897+RezaBafandeh@users.noreply.github.com>
2025-06-11 21:06:21 +0330

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import SideMenu from "./components/SideMenu";
+// temporary import used to view myOrders and details
+// import OrdersPage from "./pages/User/myOrders";
+// import OrderDetailsPage from "./pages/User/details";
+
+// const mockOrders = [
+//   {
+//     image: "https://via.placeholder.com/50",
+//     name: "Product A",
+//     quantity: 3,
+//     price: 25.99,
+//   },
+//   {
+//     image: "https://via.placeholder.com/50",
+//     name: "Product B",
+//     quantity: 1,
+//     price: 15.49,
+//   },
+// ];
 
 function App() {
   return (
     <>
-      <SideMenu>
-        
-      </SideMenu>
+      <SideMenu></SideMenu>
+      {/* return <OrdersPage />; */}
+      {/* <OrderDetailsPage orderItems={mockOrders} /> */}
     </>
   );
 }
