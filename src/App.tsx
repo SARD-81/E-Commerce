07567df0277ac3980fb381c793374a2990f62199
S.Amir Reza Davarzani <116599983+SARD-81@users.noreply.github.com>
2025-06-12 20:59:37 +0330

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,14 @@
-// import SideMenu from "./components/SideMenu";
-import { Button, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
-import { useMemo, useState } from "react";
 import SideMenu from "./components/SideMenu";
-import Badge from "./components/SlideMenuReuseables/badge";
-import Heading from "./components/SlideMenuReuseables/heading";
-import TextField from "./components/SlideMenuReuseables/textField";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { theme } from "./theme";
 
 function App() {
-  const [mode, setMode] = useState<"light" | "dark">("light");
-  const theme = useMemo(() => {
-    return createTheme({
-      palette: {
-        mode,
-        background: {
-          default: mode === "dark" ? "#0F0F10" : "#EEEFF1",
-        },
-      },
-    });
-  }, [mode]);
-  const handleToggleClick = () => {
-    setMode((prev) => (prev === "light" ? "dark" : "light"));
-  };
   return (
     <>
-      <SideMenu>
-        
-      </SideMenu>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <SideMenu></SideMenu>
+      </ThemeProvider>
     </>
   );
 }
