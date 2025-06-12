1dd08c3df45fbb5ebd0d50419ad34092220e6dde
S.Amir Reza Davarzani <116599983+SARD-81@users.noreply.github.com>
2025-06-12 21:00:37 +0330

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,14 @@
 import SideMenu from "./components/SideMenu";
-// Temporary import used to view MyOrders and DetailsMain:
-// import MyOrders from "./pages/User/MyOrders";
-// import DetailsMain from "./pages/User/DetailsMain";
-
-// const mockOrders = [
-//   {
-//     image: "https://via.placeholder.com/50",
-//     name: "Product A",
-//     quantity: 3,
-//     price: 25.99,
-//   },
-//   {
-//     image: "https://via.placeholder.com/50",
-//     name: "Product B",
-//     quantity: 1,
-//     price: 15.49,
-//   },
-// ];
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { theme } from "./theme";
 
 function App() {
   return (
     <>
-      <SideMenu></SideMenu>
-      {/* return <MyOrders />; */}
-      {/* <DetailsMain orderItems={mockOrders} /> */}
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <SideMenu></SideMenu>
+      </ThemeProvider>
     </>
   );
 }
