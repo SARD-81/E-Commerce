6dab7f7f5e8fe448abf19aba1f445a325e169987
Anahita <anahita.elyasi.gorjii@gmail.com>
2025-06-12 17:53:18 +0330

    buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import SideMenu from "./components/SideMenu";
+import Badge from "./components/SlideMenuReuseables/badge";
+import Heading from "./components/SlideMenuReuseables/heading";
+import TextField from "./components/SlideMenuReuseables/textField";
 
 function App() {
   return (
     <>
       <SideMenu>
-        
+        <Badge>فروشگاه</Badge>
+        <Badge>مشاهده بیشتر ←</Badge>
+        <Heading>فیلتر دسته‌بندی</Heading>
       </SideMenu>
+      <TextField label="نام" placeholder="آیفون ۱۴" />
+      <TextField label="قیمت" placeholder="۱۰,۰۰۰" />
+
     </>
   );
 }
