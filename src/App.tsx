d872ffb5659b0746278e5763efd5172be33cda56
Maedeh Rahmani <rahmanimaedeh99@gmail.com>
2025-06-13 18:02:50 +0430

    adding product-related comp and fonts in assets folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import RelatedProducts from "./components/RelatedProducts";
 import SideMenu from "./components/SideMenu";
+
+const theme = createTheme({
+  direction: "rtl",
+  typography: {
+    fontFamily: "Vazir, Arial, sans-serif",
+  },
+});
 
 function App() {
   return (
     <>
-      <SideMenu>
-        
-      </SideMenu>
+      <ThemeProvider theme={theme}>
+        <SideMenu></SideMenu>
+        <CssBaseline />
+        <RelatedProducts />
+      </ThemeProvider>
     </>
   );
 }
