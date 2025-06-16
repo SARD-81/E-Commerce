d27bddd1c45e35b80b1940d93233941f8b915cc6
Sara <sarahpiri992@gmail.com>
2025-06-16 15:57:29 +0200

    Add axios for API requests and update App component state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import SideMenu from "./components/SideMenu";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import { theme } from "./theme";
+import { useState } from "react";
 
 function App() {
+  const [Response, SetResponse] = useState({
+    loading: true,
+    error: false,
+    data: null,
+  });
+
   return (
     <>
       <ThemeProvider theme={theme}>
