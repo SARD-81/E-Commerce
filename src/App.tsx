8e5c36c28cc2ecb0c72ca6a2d10a4d01c98b7a0a
alishf <ali88206497@gmail.com>
2025-06-28 00:59:14 +0330

    changing toggle mode

--- src/App.tsx
+++ src/App.tsx
@@ -3,56 +3,19 @@ import RelatedProducts from "./components/RelatedProducts";
 import SideMenu from "./components/SideMenu";
 import { theme } from "./theme";
 
-
-const theme = createTheme({
-  direction: "rtl",
-  typography: {
-    fontFamily: "Vazir, Arial, sans-serif",
-  },
-});
+// const theme = createTheme({
+//   direction: "rtl",
+//   typography: {
+//     fontFamily: "Vazir, Arial, sans-serif",
+//   },
+// });
 
 function App() {
-  const [mode, setMode] = useState<"light" | "dark">("light");
-  const theme = useMemo(() => {
-    return createTheme({
-      palette: {
-        mode,
-        background: {
-          default: mode === "dark" ? "#0F0F10" : "#EEEFF1",
-        },
-      },
-    });
-  }, [mode]);
-  const handleToggleClick = () => {
-    setMode((prev) => (prev === "light" ? "dark" : "light"));
-  };
   return (
     <>
       <ThemeProvider theme={theme}>
-
         <CssBaseline />
-<<<<<<< HEAD
-        <Button
-          variant="outlined"
-          onClick={handleToggleClick}
-          sx={{
-            position: "fixed",
-            top: 20,
-            left: 20,
-            zIndex: 999,
-            backgroundColor: "Background.paper",
-          }}
-        >
-          {mode === "light" ? "Dark Mode" : "Light Mode"}
-        </Button>
-        <SideMenu>
-          {/* <ProductPage /> */}
-          {/* <ProductCreate /> */}
-        </SideMenu>
-=======
-        <SideMenu></SideMenu>
-
->>>>>>> 390c6d74fac228ddc26799b05b439b97a1f0c4bb
+        <SideMenu>{/* <DarkMode /> */}</SideMenu>
       </ThemeProvider>
     </>
   );
---
+++ src/darkMode.tsx
@@ -0,0 +1,36 @@
+import { Button, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { useMemo, useState } from "react";
+import { FiSun } from "react-icons/fi";
+import { FiMoon } from "react-icons/fi";
+const DarkMode = () => {
+  const [mode, setMode] = useState<"light" | "dark">("light");
+  const theme = useMemo(() => {
+    return createTheme({
+      palette: {
+        mode,
+        background: {
+          default: mode === "dark" ? "#0F0F10" : "EEEFF1",
+        },
+      },
+    });
+  }, [mode]);
+  const handleToggleClick = () => {
+    setMode((prev) => (prev === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <button
+        onClick={handleToggleClick}
+        className={`cursor-pointer fixed top-7 left-7 z-999 text-3xl transition-all duration-300 ease-in-out ${
+          mode === "light" ? "text-[#1A237E]" : "text-[#FFEB3B]"
+        }`}
+      >
+        {mode === "light" ? <FiMoon /> : <FiSun />}
+      </button>
+    </ThemeProvider>
+  );
+};
+
+export default DarkMode;
