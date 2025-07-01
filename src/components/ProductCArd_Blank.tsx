706fc6800df4332c492e547c97ecb419409f0200
Sara <sarahpiri992@gmail.com>
2025-07-01 21:45:43 +0200

    Refactor ProductCArd_Blank component to use MUI Box and Typography for improved styling

--- src/components/ProductCArd_Blank.tsx
+++ src/components/ProductCArd_Blank.tsx
@@ -1,3 +1,5 @@
+import { Box, Typography } from "@mui/material";
+
 interface ProductCard_BlankProps {
   productId: number | string;
   title: string;
@@ -17,25 +19,35 @@ const ProductCArd_Blank = ({
       : { width: 250, height: 250 };
 
   return (
-    <div
+    <Box
       style={{
         width: imageSize.width,
       }}
     >
-      <img
+      <Box
+        component="img"
         className="rounded-xl mb-4 bg-[#797979]"
         src={imageSrc}
         alt="product_card_blank"
         width={imageSize.width}
         height={imageSize.height}
       />
-      <div className="flex justify-between items-center ">
-        <p className="font-bold">{title}</p>
-        <div className="bg-[#831747]  px-2 rounded-full">
-          <p className="text-white text-sm">{price} تومان</p>
-        </div>
-      </div>
-    </div>
+
+      <Box className="flex justify-between items-center ">
+        <Typography component="p" variant="body1" className="font-bold">
+          {title}
+        </Typography>
+        <Box className="bg-[#831747]  px-2 rounded-full">
+          <Typography
+            component="p"
+            variant="body1"
+            className="text-white text-sm"
+          >
+            {price} تومان
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
