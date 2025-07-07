f8a9c0e991784117a9e43c7429f48ecb5a0117a1
Anahita <anahita.elyasi.gorjii@gmail.com>
2025-07-07 18:50:53 +0330

    added favorite file

---
+++ src/pages/User/favorite.tsx
@@ -0,0 +1,10 @@
+
+
+const Favorite = () => {
+    return(
+        <div></div>
+    );
+}
+{/* <wProductCard productId="product" title="Apple pro" price="10000" description="nothing" imageSrc="" onShowMore="" onAddToBasket=""/> */}
+
+export default Favorite;
